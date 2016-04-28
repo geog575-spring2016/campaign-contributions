5b1148f98487630b14792cbe7f9de13b278cce0d
kristenvincent <kvincent2@wisc.edu>
2016-04-28 14:42:50 -0500

    Create Split Prop Symbols for One Set of Data

--- js/main.js
+++ js/main.js
@@ -2,16 +2,18 @@ var attrArray = ["Bobby", "CarlyFiorina","ChrisChristie", "GeorgePataki",  "Jame
 var expressed = attrArray[0];
 var width = window.innerWidth * 0.645,
     height = 650;
+var svg;
+var map;
+var projection;
+var setRadius;
 
 
 window.onload = setMap();
 
 // set the width and height of the map
 function setMap() {
-
-
     // creating the map as an svg and giving it attributes of width and height
-    var map = d3.select("body")
+    map = d3.select("body")
         .append("svg")
         .attr("class", "map")
         .attr("width", width)
@@ -22,7 +24,7 @@ function setMap() {
         .html("We "+"don't".strike()+" need your money, money, money");
 
     // creating projection - Albers USA which puts Alaska and Hawaii, projected in their own right below the 48 contiguous states
-    var projection = d3.geo.albersUsa()
+    projection = d3.geo.albersUsa()
     // no center because it's already centered on the US as part of the projection code
         .scale(1000)
         .translate([width / 2, height / 2]); // keeps map centered in the svg container
@@ -39,21 +41,28 @@ function setMap() {
 
     // callback function that takes the data as two parameters and an error parameter that will report any errors that occur
     function callback(error, bobby, unitedStates) {
+        bobby.forEach(function(d){
+          d.Lat= +d.Lat
+          d.Lon= +d.Lon
+          d.Bobby = +d.Bobby
+        })
         // translate the topojson to GeoJSON within the DOM
         var us = topojson.feature(unitedStates, unitedStates.objects.US_shapefile).features; // pulls the array of features from the shapefile data and passes it to .data()
 
          us = joinData(us, bobby);
-      
-        setEnumerationUnits(us, map, path);
+
+
+        setEnumerationUnits(us, path);
         // setCircles (path,map,bobby,projection);
-        createDropdown(bobby);
-        createSplitSymbols(bobby);
+        //createDropdown(bobby);
+        createSplitSymbols(bobby, us);
 
     };
+    //createSplitSymbols(bobby);
 };
 
 function joinData(us, bobby){
-   
+
     //loop through csv to assign each set of csv attribute values to geojson region
     for (var i=0; i<bobby.length; i++){
         var csvRegion = bobby[i]; //the current region
@@ -63,7 +72,7 @@ function joinData(us, bobby){
         for (var a=0; a<us.length; a++){
             var geojsonProps = us[a].properties; //the current region geojson properties
             var geojsonKey = geojsonProps.postalcode; //the geojson primary key
-           
+
             //where primary keys match, transfer csv data to geojson properties object
             if (geojsonKey == csvKey){
 
@@ -76,12 +85,12 @@ function joinData(us, bobby){
             };
         };
     };
- 
+
     return us;
 
 };
 
-function setEnumerationUnits(us, map, path){
+function setEnumerationUnits(us, path){
 
 var states = map.selectAll(".states")
             .data(us)
@@ -94,58 +103,59 @@ var states = map.selectAll(".states")
 };
 
 
-
+//
 // function setCircles (path,map,bobby,projection){
-
+//
 //          var circles = map.selectAll(".circles")
 //         .data(bobby)
 //         .enter()
 //         .append("circle")
 //         .attr("class", function(d){
-             
+//
 //             return "circles " + d.postalcode;
 //         })
 //         .attr("fill", "grey")
 //         .attr('fill-opacity', 0.5)
 //         .attr("cx", function(d) {
-//             return projection([d.Lon, d.Lat])[0]; }) 
+//             return projection([d.Lon, d.Lat])[0]; })
 //         .attr("cy", function(d) { return projection([d.Lon, d.Lat])[1]; });
-
+//
 //         updateCircles(circles,bobby);
- 
-
+//         createSplitSymbols(bobby);
+//
+//
 // };
 
-function createDropdown(bobby){
-    //add select element
-    var dropdown = d3.select("body")
-        .append("select")
-        .attr("class", "dropdown")
-        .on("change", function(){
-            changeAttribute(this.value, bobby)
-        });
-
-    //add initial option
-    var titleOption = dropdown.append("option")
-        .attr("class", "titleOption")
-        .attr("disabled", "true")
-        .text("Pick a candidate");
-
-    //add attribute name options
-    var attrOptions = dropdown.selectAll("attrOptions")
-        .data(attrArray)
-        .enter()
-        .append("option")
-        .attr("value", function(d){ return d })
-        .text(function(d){ return d });
-};
+// function createDropdown(bobby){
+//     //add select element
+//     var dropdown = d3.select("body")
+//         .append("select")
+//         .attr("class", "dropdown")
+//         .on("change", function(){
+//             changeAttribute(this.value, bobby)
+//         });
+//
+//     //add initial option
+//     var titleOption = dropdown.append("option")
+//         .attr("class", "titleOption")
+//         .attr("disabled", "true")
+//         .text("Pick a candidate");
+//
+//     //add attribute name options
+//     var attrOptions = dropdown.selectAll("attrOptions")
+//         .data(attrArray)
+//         .enter()
+//         .append("option")
+//         .attr("value", function(d){ return d })
+//         .text(function(d){ return d });
+// };
 
 function changeAttribute(attribute, bobby){
     //change the expressed attribute
     expressed = attribute;
     var circles = d3.selectAll(".circles");
     updateCircles(circles,bobby);
-  
+
 };
 
 
@@ -158,32 +168,50 @@ function updateCircles(circles, bobby) {
     var radiusMin = Math.min.apply(Math, domainArray);
     var radiusMax = Math.max.apply(Math, domainArray);
 
-    setRadius = d3.scale.sqrt()
-            .range([4, 40])
-            .domain([radiusMin, radiusMax]);
+    // setRadius = d3.scale.sqrt()
+    //         .range([4, 40])
+    //         .domain([radiusMin, radiusMax]);
         //create a second svg element to hold the bar chart
     var circleRadius= circles.attr("r", function(d){
-                return setRadius(d[expressed]); 
+                return setRadius(d[expressed]);
             });
+    createSplitSymbols(setRadius);
     };
 
 //Kristen's Section
 //Create Split Prop Symbols
-// function createSplitSymbols(splits, bobby){
-//     var arc = d3.svg.arc()
-//         .innerRadius(0)
-//         .outerRadius(function(d){
-//             return radiusFlannery(d.bobby)
-//         })
-//         .startAngle(0)
-//         .endAngle(Math.PI)
+function createSplitSymbols(bobby, us){
+    var arc = d3.svg.arc()
+        .innerRadius(0)
+        .outerRadius(function(d){
+          console.log(setRadius(d.Bobby));
+            return setRadius(d.Bobby)
+        })
+        .startAngle(Math.PI)
+        .endAngle(Math.PI*2)
+      console.log(bobby);
 
-//     var candidate1 = svg.append("g")
-//     candidate1.selectAll("path")
-//         .data(bobby)
-//         .enter().append("path")
-//         .style("fill", defaultColor)
-//         .style(defaultStroke)
-//         .attr("transform", function(d){
-//             return "translate(" + projection([d.])
-//         })
+      radiusMin = d3.min(bobby, function(d){
+        return d.Bobby
+      })
+
+      radiusMax = d3.max(bobby, function(d){
+        return d.Bobby
+      })
+      console.log(radiusMax);
+      setRadius = d3.scale.sqrt()
+              .domain([radiusMin, radiusMax])
+              .range([4, 40]);
+
+    var candidate1 = map.append("g");
+    candidate1.selectAll("path")
+        .data(bobby)
+        .enter().append("path")
+        .style("fill", "blue")
+        .style("stroke", "red")
+        //length of line
+        .attr("transform", function(d){
+            return "translate(" + projection([d.Lon, d.Lat])[0] + "," + projection([d.Lon, d.Lat])[1]+")";
+        })
+        .attr("d", arc);
+};
