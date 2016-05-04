b413ccdc3c6aa34f90c41a1a85492f6b29e0952f
kristenvincent <kvincent2@wisc.edu>
2016-05-04 17:26:42 -0500

    work on adding split symbols

--- js/main.js
+++ js/main.js
@@ -3,14 +3,21 @@ var expressed = attrArray[0];
 var attrArray2 = ["March_15", "April_15","May_15", "June_15",  "July_15","August_15","September_15","October_15","November_15","December_15","January_16","February_16","March_16"];
 var expressed2 = attrArray2[0];
 var map;
+var svg;
+var projection;
+//var setRadius;
 var radioName = expressed;
 var checkedArray = [];
+var width = window.innerWidth * 0.645,
+    height = 650;
+var attributeNames = [];
+var csvArray = [];
 
 window.onload = setMap();
 // set the width and height of the map
 function setMap() {
-  var width = window.innerWidth * 0.645,
-        height = 650;
+  // var width = window.innerWidth * 0.645,
+  //       height = 650;
 
     // creating the map as an svg and giving it attributes of width and height
     map = d3.select("#mapContainer")
@@ -25,7 +32,7 @@ function setMap() {
     //     .html("We "+"don't".strike()+" need your money, money, money");
 
     // creating projection - Albers USA which puts Alaska and Hawaii, projected in their own right below the 48 contiguous states
-    var projection = d3.geo.albersUsa()
+    projection = d3.geo.albersUsa()
     // no center because it's already centered on the US as part of the projection code
         .scale(1000)
         .translate([width / 2, height / 2]); // keeps map centered in the svg container
@@ -37,26 +44,92 @@ function setMap() {
     // uses queue.js to parallelize asynchronous loading of the the CSV and shapefile data
     d3_queue.queue()
         .defer(d3.csv, "data/total_contributions_percandidate_perstate.csv")
+        .defer(d3.csv, "data/GoodCSVs/BenCarson.csv")
+        .defer(d3.csv, "data/GoodCSVs/BernieSanders.csv")
         .defer(d3.csv, "data/GoodCSVs/BobbyJindal.csv")
-        .defer(d3.csv, "data/GoodCSVs/TedCruz.csv")
-        .defer(d3.csv, "data/GoodCSVs/LawrenceLessig.csv")
         .defer(d3.json, "data/US_shapefile.topojson")
+        .defer(d3.csv, "data/GoodCSVs/CarlyFiorina.csv")
+        .defer(d3.csv, "data/GoodCSVs/ChrisChristie.csv")
+        .defer(d3.csv, "data/GoodCSVs/DonaldTrump.csv")
+        .defer(d3.csv, "data/GoodCSVs/GeorgePataki.csv")
+        .defer(d3.csv, "data/GoodCSVs/HillaryClinton.csv")
+        .defer(d3.csv, "data/GoodCSVs/JamesWebb.csv")
+        .defer(d3.csv, "data/GoodCSVs/JebBush.csv")
+        .defer(d3.csv, "data/GoodCSVs/JohnKasich.csv")
+        .defer(d3.csv, "data/GoodCSVs/LawrenceLessig.csv")
+        .defer(d3.csv, "data/GoodCSVs/LindseyGraham.csv")
+        .defer(d3.csv, "data/GoodCSVs/MarcoRubio.csv")
+        .defer(d3.csv, "data/GoodCSVs/MartinOMalley.csv")
+        .defer(d3.csv, "data/GoodCSVs/MikeHuckabee.csv")
+        .defer(d3.csv, "data/GoodCSVs/RandPaul.csv")
+        .defer(d3.csv, "data/GoodCSVs/RickPerry.csv")
+        .defer(d3.csv, "data/GoodCSVs/RickSantorum.csv")
+        .defer(d3.csv, "data/GoodCSVs/ScottWalker.csv")
+        .defer(d3.csv, "data/GoodCSVs/TedCruz.csv")
         .await(callback); // waits til both sets of data are loaded before it sends the data to the callback function
 
     // callback function that takes the data as two parameters and an error parameter that will report any errors that occur
     function callback(error, total, test1, test2, test3, unitedStates) {
+      total.forEach(function(d) {
+        d.Lat= +d.Lat
+        d.Lon= +d.Lon
+        d.DonaldTrump = +d.DonaldTrump
+        d.HillaryClinton = +d.HillaryClinton
+      })
+      test1.forEach(function(d){
+        d.Lat= +d.Lat
+        d.Lon= +d.Lon
+        d.state_total= +d.state_total
+        // d.BenCarson = +d.BenCarson
+        // d.BernieSanders = +d.BernieSanders
+        // d.BobbyJindal = +d.BobbyJindal
+        // d.CarlyFiorina = +d.CarlyFiorina
+        // d.ChrisChristie = +d.ChrisChristie
+        // d.DonaldTrump = +d.DonaldTrump
+        // d.GeorgePataki = +d.GeorgePataki
+        // d.HillaryClinton = +d.HillaryClinton
+        // d.JamesWebb = +d.JamesWebb
+        // d.JebBush = +d.JebBush
+        // d.JohnKasich = +d.JohnKasich
+        // d.LawrenceLessig= +d.LawrenceLessig
+        // d.LindseyGraham = +d.LindseyGraham
+        // d.MarcoRubio = +d.MarcoRubio
+        // d.MartinOMalley = +d.MartinOMalley
+        // d.MikeHuckabee = +d.MikeHuckabee
+        // d.RandPaul = +d.RandPaul
+        // d.RickPerry = +d.RickPerry
+        // d.RickSantorum = +d.RickSantorum
+        // d.ScottWalker= +d.ScottWalker
+        // d.TedCruz = +d.TedCruz
+      });
+
+      test2.forEach(function(d){
+        d.Lat= +d.Lat
+        d.Lon= +d.Lon
+        d.state_total= +d.state_total
+      });
+
+      test3.forEach(function(d){
+        d.Lat= +d.Lat
+        d.Lon= +d.Lon
+        d.state_total= +d.state_total
+
+      });
         // translate the topojson to GeoJSON within the DOM
         var us = topojson.feature(unitedStates, unitedStates.objects.US_shapefile).features; // pulls the array of features from the shapefile data and passes it to .data()
-        var csvArray = [total, test1, test2, test3];
-        var attributeNames = ["totalcontribution", "test1contribution", "test2contribution", "test3contribution"];
+        //console.log(us);
+        csvArray = [total, test1, test2, test3];
+        //console.log(csvArray[0]);
+        attributeNames = ["total", "Ben Carson", "Bernie Sanders", "Bobby Jindal"];
            for (csv in csvArray){
             joinData(us, csvArray[csv], attributeNames[csv]);
 
         };
          setEnumerationUnits(us, map, path);
          setCircles (path,map,total,projection);
-        //  createDropdown(bobby);
-         createradio(total,path,map,test1,test2,test3,projection);
+        //createDropdown(bobby);
+         createradio(total,path,map,test1,test2,test3,projection,total,us);
+         //createSplitSymbols(total,candidate1,candidate2,us);
     };
 };
 
@@ -101,8 +174,6 @@ var states = map.selectAll(".states")
             .attr("d", path);
 };
 
-
-
 function setCircles (path,map,data,projection){
 
          var circles = map.selectAll(".circles")
@@ -118,23 +189,19 @@ function setCircles (path,map,data,projection){
         .attr("cx", function(d) {
             return projection([d.Lon, d.Lat])[0]; })
         .attr("cy", function(d) { return projection([d.Lon, d.Lat])[1]; });
-
-
         updateCircles(circles,data);
-
-
 };
 
-function createradio(data,path,map,test1,test2,test3,projection){
+function createradio(data,path,map,test1,test2,test3,projection,total,us){
 
     var filterPhases = ["Total", "Per Capita"],
     j=0;
-
+//console.log(projection);
     var form1 = d3.select("#sideColumn")
     .append("form")
     .attr("class", "Classification1")
   //  .text("Category 1:");
-    console.log(form1)
+    //console.log(form1)
 
      var labelEnter = form1.selectAll("span")
     .data(filterPhases)
@@ -152,8 +219,7 @@ function createradio(data,path,map,test1,test2,test3,projection){
     .on("change", function(d){
             changeAttribute(this.value, data);
             radioName = d;
-            console.log(radioName);
-
+            //console.log(radioName);
         })
 
     .property("checked", function(d, i) {return i===j;})
@@ -161,50 +227,22 @@ function createradio(data,path,map,test1,test2,test3,projection){
     labelEnter.append("label").text(function(d) {return d;});
 
 
-    createcheckbox(path,map,test1,test2,test3,projection);
-
+    createcheckbox(total, us, projection);
 };
 
 
-// function createDropdown(data){
-//     //add select element
-//     var dropdown = d3.select("#sideColumn")
-//         .append("select")
-//         .attr("class", "dropdown")
-//         .on("change", function(){
-//             changeAttribute(this.value, data)
-//         });
-//
-//     //add initial option
-//     var titleOption = dropdown.append("option")
-//         .attr("class", "titleOption")
-//         .attr("disabled", "true")
-//         .text("Pick a candidate");
-//
-//     //add attribute name options
-//     var attrOptions = dropdown.selectAll("attrOptions")
-//         .data(attrArray)
-//         .enter()
-//         .append("option")
-//         .attr("value", function(d){ return d })
-//         .text(function(d){ return d });
-// };
+function createcheckbox(total, us, projection){
 
-function createcheckbox(path,map,test1,test2,test3,projection){
-
-    var candidatesNames = ["Bobby Jindal", "Ted Cruz","Lawrence Lessig"];
-
-
+    var candidatesNames = ["Ben Carson", "Bernie Sanders","Bobby Jindal"];
+    //console.log(projection);
     var form2 = d3.select("#sideColumn")
     .append("form")
     .attr("class", "Classification2");
-
 
      var labelEnter = form2.selectAll("span")
     .data(candidatesNames)
     .enter()
     .append("span");
-
 
     labelEnter.append("input")
     .attr({
@@ -215,6 +253,7 @@ function createcheckbox(path,map,test1,test2,test3,projection){
         value: function(d) {return d;}
     })
     .on("click", function(d){
+      //console.log(projection);
 
 
 if(checkedArray.length == 0)
@@ -255,10 +294,13 @@ else
 }
 
     if (checkedArray.length == 2)
-{console.log("Using "+ checkedArray[0] + " and " + checkedArray[1]+" to make split symbols showing "+ radioName);
-}
+    {createSplitSymbols(total,checkedArray[0],checkedArray[1], us,projection);}
+
+    // {console.log("Using "+ checkedArray[0] + " and " + checkedArray[1]+" to make split symbols showing "+ radioName);}
+
 else if (checkedArray.length ==1)
-    {console.log("Using "+ checkedArray[0] +" to make symbols showing "+ radioName);}
+    {createSplitSymbols(total,checkedArray[0],checkedArray[1], us,projection);}
+  //{console.log("Using "+ checkedArray[0] +" to make symbols showing "+ radioName)}
 
 else if (checkedArray.length ==0)
     {console.log("Only showing "+ radioName+ " in each state");}
@@ -268,9 +310,6 @@ else if (checkedArray.length ==0)
     labelEnter.append("label").text(function(d) {return d;});
 };
 
-
-
-
 function changeAttribute(attribute, data){
     //change the expressed attribute
     expressed = attribute;
@@ -279,7 +318,6 @@ function changeAttribute(attribute, data){
 
 };
 
-
 function updateCircles(circles, data)
 {
     var domainArray = [];
@@ -287,100 +325,171 @@ function updateCircles(circles, data)
         var val = parseFloat(data[i][expressed]);
         domainArray.push(val);
     };
-        var radiusMin = Math.min.apply(Math, domainArray);
-        var radiusMax = Math.max.apply(Math, domainArray);
-        console.log(radiusMax);
-var setRadius = d3.scale.sqrt()
-        .range([4, 40])
-        .domain([radiusMin, radiusMax]);
+        radiusMin = Math.min.apply(Math, domainArray);
+        radiusMax = Math.max.apply(Math, domainArray);
+        //console.log(radiusMax);
+        setRadius = d3.scale.sqrt()
+            .range([4, 40])
+            .domain([radiusMin, radiusMax]);
     //create a second svg element to hold the bar chart
 var circleRadius= circles.attr("r", function(d){
             return setRadius(d[expressed]);
         });
+        //createSplitSymbols(setRadius);
 };
 
-// function overlay(path,map,test1,test2,test3,projection){
-//     $(".test1-section").click(function(){
-//         var test1Div = document.getElementById('test1-centers');
-//         var test1InsetDiv = document.getElementById('test1-inset');
-//         if (d3.selectAll(".circles")[0].length > 0){
-//             removeCPC = d3.selectAll(".circles").remove();
-//             test1InsetDiv.style.visibility = "hidden";
-//         } else {
-//             setCircles2(path,map,test1,projection);
-//             test1InsetDiv.style.visibility = "visible";
-//         }
-//     });
-//
-//         $(".test2-section").click(function(){
-//         var test2Div = document.getElementById('test2-centers');
-//         var test2InsetDiv = document.getElementById('test2-inset');
-//         if (d3.selectAll(".circles")[0].length > 0){
-//             removeCPC = d3.selectAll(".circles").remove();
-//             test2InsetDiv.style.visibility = "hidden";
-//         } else {
-//             setCircles2(path,map,test2,projection);
-//             test2InsetDiv.style.visibility = "visible";
-//         }
-//     });
-//
-//     $(".test3-section").click(function(){
-//         var test3Div = document.getElementById('test3-centers');
-//         var test3InsetDiv = document.getElementById('test3-inset');
-//         if (d3.selectAll(".circles")[0].length > 0){
-//             removeCPC = d3.selectAll(".circles").remove();
-//             test3InsetDiv.style.visibility = "hidden";
-//         } else {
-//             setCircles2(path,map,test3,projection);
-//             test3InsetDiv.style.visibility = "visible";
-//         }
-//     });
-// }; //END overlay function
-//
-//
-// function setCircles2 (path,map,data,projection){
-//
-//          var circles = map.selectAll(".circles")
-//         .data(data)
-//         .enter()
-//         .append("circle")
-//         .attr("class", function(d){
-//
-//             return "circles " + d.state;
-//         })
-//         .attr("fill", "red")
-//         .attr('fill-opacity', 0.5)
-//         .attr("cx", function(d) {
-//             return projection([d.Lon, d.Lat])[0]; })
-//         .attr("cy", function(d) { return projection([d.Lon, d.Lat])[1]; });
-//
-//         updateCircles2(circles,data);
-//
-// };
-//
-// function updateCircles2(circles, data)
-// {
-//     var domainArray = [];
-//     for (var i=0; i<data.length; i++){
-//         var val = parseFloat(data[i][expressed2]);
-//         domainArray.push(val);
-//     };
-//         var radiusMin = Math.min.apply(Math, domainArray);
-//         var radiusMax = Math.max.apply(Math, domainArray);
-//
-// var setRadius = d3.scale.sqrt()
-//         .range([4, 40])
-//         .domain([radiusMin, radiusMax]);
-//     //create a second svg element to hold the bar chart
-// var circleRadius= circles.attr("r", function(d){
-//             return setRadius(d[expressed2]);
-//         });
-// };
+function createSplitSymbols(total,candidate1,candidate2,us,projection){
+  //loop through csv to assign each set of csv attribute values to geojson region
+  //console.log(projection);
+  var firstCandidate;
+  var secondCandidate;
+  //console.log(csvArray);
+  for (var i=0; i<csvArray.length; i++){
+    for (var j=0; j<csvArray[i].length; j++) {
+      //console.log(csvArray[i][j].state);
+        if (attributeNames[i] == candidate1){
 
-//function to create info panel
-// function createInfoPanel(panel) {
-//   d3.select("#infoPanel")
-//       //.append("select")
-//       //.attr("class", "col-md-2")
-//
-// };
+          //add j!!
+          firstCandidate = csvArray[i][j];
+          var arc = d3.svg.arc()
+              .innerRadius(0)
+              .outerRadius(function(d){
+                //console.log(a);
+                  return setRadius(firstCandidate.state_total)
+              })
+              .startAngle(Math.PI)
+              .endAngle(Math.PI*2)
+            //console.log(totals);
+
+            radiusMin = d3.min(firstCandidate, function(d){
+              return firstCandidate.state_total
+            })
+
+            radiusMax = d3.max(firstCandidate, function(d){
+              return firstCandidate.state_total
+            })
+            //console.log(radiusMax);
+            setRadius = d3.scale.sqrt()
+                    .domain([radiusMin, radiusMax])
+                    .range([4, 40]);
+
+          var candidatea = map.append("g");
+          candidatea.selectAll("path")
+              .data(firstCandidate)
+              .enter().append("path")
+              .style("fill", "blue")
+              .style("stroke", "red")
+              //length of line
+              .attr("transform", function(d){
+                  return "translate(" + projection([d.Lon, d.Lat])[0] + "," + projection([d.Lon, d.Lat])[1]+")";
+              })
+              .attr("d", arc);
+        }
+        else if (attributeNames[i] == candidate2) {
+          secondCandidate = csvArray[i][j];
+          var arc2 = d3.svg.arc()
+              .innerRadius(0)
+              .outerRadius(function(d){
+                //console.log(setRadius(d.HillaryClinton));
+                  return setRadius(b.state_total)
+              })
+              .startAngle(0)
+              .endAngle(Math.PI)
+            //console.log(totals);
+
+            radiusMin = d3.min(b, function(d){
+              return secondCandidate.state_total
+            })
+
+            radiusMax = d3.max(b, function(d){
+              return secondCandidate.state_total
+            })
+            //console.log(radiusMax);
+            setRadius = d3.scale.sqrt()
+                    .domain([radiusMin, radiusMax])
+                    .range([4, 40]);
+
+          var candidateb = map.append("g");
+          candidateb.selectAll("path")
+              .data(secondCandidate)
+              .enter().append("path")
+              .style("fill", "blue")
+              .style("stroke", "red")
+              //length of line
+              .attr("transform", function(d){
+                  return "translate(" + projection([d.Lon, d.Lat])[0] + "," + projection([d.Lon, d.Lat])[1]+")";
+              })
+              .attr("d", arc2);
+        }
+      }
+    };
+  //split symbol for first candidate
+    // var arc = d3.svg.arc()
+    //     .innerRadius(0)
+    //     .outerRadius(function(d){
+    //       //console.log(setRadius(d.BenCarson));
+    //         return setRadius(a.state_total)
+    //     })
+    //     .startAngle(Math.PI)
+    //     .endAngle(Math.PI*2)
+    //   //console.log(totals);
+    //
+    //   radiusMin = d3.min(total, function(d){
+    //     return a.state_total
+    //   })
+    //
+    //   radiusMax = d3.max(total, function(d){
+    //     return a.state_total
+    //   })
+    //   //console.log(radiusMax);
+    //   setRadius = d3.scale.sqrt()
+    //           .domain([radiusMin, radiusMax])
+    //           .range([4, 40]);
+    //
+    // var candidate1 = map.append("g");
+    // candidate1.selectAll("path")
+    //     .data(totals)
+    //     .enter().append("path")
+    //     .style("fill", "blue")
+    //     .style("stroke", "red")
+    //     //length of line
+    //     .attr("transform", function(d){
+    //         return "translate(" + projection([d.Lon, d.Lat])[0] + "," + projection([d.Lon, d.Lat])[1]+")";
+    //     })
+    //     .attr("d", arc);
+
+    //start split symbol for second candidate
+    // var arc2 = d3.svg.arc()
+    //     .innerRadius(0)
+    //     .outerRadius(function(d){
+    //       //console.log(setRadius(d.HillaryClinton));
+    //         return setRadius(b.state_total)
+    //     })
+    //     .startAngle(0)
+    //     .endAngle(Math.PI)
+    //   //console.log(totals);
+    //
+    //   radiusMin = d3.min(total, function(d){
+    //     return b.state_total
+    //   })
+    //
+    //   radiusMax = d3.max(total, function(d){
+    //     return b.state_total
+    //   })
+    //   //console.log(radiusMax);
+    //   setRadius = d3.scale.sqrt()
+    //           .domain([radiusMin, radiusMax])
+    //           .range([4, 40]);
+    //
+    // var candidate2 = map.append("g");
+    // candidate2.selectAll("path")
+    //     .data(totals)
+    //     .enter().append("path")
+    //     .style("fill", "blue")
+    //     .style("stroke", "red")
+    //     //length of line
+    //     .attr("transform", function(d){
+    //         return "translate(" + projection([d.Lon, d.Lat])[0] + "," + projection([d.Lon, d.Lat])[1]+")";
+    //     })
+    //     .attr("d", arc2);
+};
